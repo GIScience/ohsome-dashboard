70c85181b925c41aaf2a1586fd19643979aa4d32
Michael Auer <michael.auer@heigit.org>
2023-07-14 10:19:47 +0200

    feat: display user info in case oqt service is unavailable
    if the metadata request fails more than 3 times, an unavailability message will be displayed in the OQT Query Panel

--- src/app/oqt/oqt-api-metadata-provider.service.ts
+++ src/app/oqt/oqt-api-metadata-provider.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {OqtApiService} from './oqt-api.service';
-import {catchError, of, tap} from 'rxjs';
+import {catchError, retry, tap, throwError} from 'rxjs';
 import {MetadataResponseJSON} from './types/MetadataResponseJSON';
 
 @Injectable({
@@ -8,33 +8,46 @@ import {MetadataResponseJSON} from './types/MetadataResponseJSON';
 })
 export class OqtApiMetadataProviderService {
   private oqtMetadataResponse: MetadataResponseJSON;
+  public oqtApiAvailable = false;
 
-  constructor(private oqtApi: OqtApiService) {}
-
-  getOqtApiMetadata() {
-     return this.oqtMetadataResponse;
+  constructor(private oqtApi: OqtApiService) {
   }
 
-  loadOqtApiMetadata(){
+  getOqtApiMetadata() {
+    return this.oqtMetadataResponse;
+  }
+
+  loadOqtApiMetadata() {
     return this.oqtApi.getMetadata()
       .pipe(
-      tap(
-        response => {
-          console.log(response);
-          // check if response contains a result
-          if ('result' in response){
-            this.oqtMetadataResponse = response;
+        retry(3),
+        tap(
+          response => {
+            console.log(response);
+            // check if response contains a result
+            if ('result' in response) {
+              this.oqtMetadataResponse = response;
+              this.oqtApiAvailable = true;
+            } else {
+              this.oqtApiAvailable = false;
+            }
           }
-          else {
-            alert("Ohsome Quality Analyst Service did not respond with a metadata response.");
+        ),
+        catchError(error => {
+          this.oqtApiAvailable = false;
+          if (error.status === 0) {
+            // A client-side or network error occurred. Handle it accordingly.
+            console.error('An error occurred:', error.error);
+          } else {
+            // The backend returned an unsuccessful response code.
+            // The response body may contain clues as to what went wrong.
+            console.error(
+              `Backend returned code ${error.status}, body was: `, error.error);
           }
-        }
-      ),
-      catchError(err => {
-        alert("Ohsome Quality Analyst Service did not respond with a metadata response.");
-        return of({"message": "Ohsome Quality Analyst Service did not respond with a metadata response."})
-      } )
-    );
+          // Return an observable with a user-facing error message.
+          return throwError(() => new Error('Ohsome Quality Analyst Service did not respond with a metadata response.'));
+        })
+      );
 
   }
 
