b89d1f7469336ac5134c2abc199ff168ffbab50d
Michael Auer <michael.auer@heigit.org>
2023-07-17 10:59:02 +0200

    test: fix tests for AppComponent

--- src/app/oshdb/ohsome-api-metadata-provider.service.mock.ts
+++ src/app/oshdb/ohsome-api-metadata-provider.service.mock.ts
@@ -6,10 +6,15 @@ const OhsomeApiMetadataProviderServiceMock = createSpyObj(
   'MetadataProviderService',
   [
     'loadOhsomeMetaData',
-    'getOhsomeMetadataResponse'
+    'getOhsomeMetadataResponse',
+    'hasOhsomeApiAnnouncement',
+    'getOhsomeApiAnnouncement'
   ]);
 
 OhsomeApiMetadataProviderServiceMock.loadOhsomeMetaData.and.returnValue(of(ohsomeApiMetadataResponse))
 OhsomeApiMetadataProviderServiceMock.getOhsomeMetadataResponse.and.returnValue(ohsomeApiMetadataResponse);
+OhsomeApiMetadataProviderServiceMock.hasOhsomeApiAnnouncement.and.returnValue(true);
+OhsomeApiMetadataProviderServiceMock.getOhsomeApiAnnouncement.and.returnValue({"Announce": "Hello World"});
+
 
 export default OhsomeApiMetadataProviderServiceMock;
