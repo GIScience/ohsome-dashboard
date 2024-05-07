0e585f25ea412ab61161427e5d250ba8559bff9d
Michael Auer <michael.auer@heigit.org>
2024-05-07 14:56:46 +0200

    test: add tests on OhsomeApiService

--- src/app/oshdb/ohsome-api.service.spec.ts
+++ src/app/oshdb/ohsome-api.service.spec.ts
@@ -1,17 +1,58 @@
-import {TestBed, inject} from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
 import {OhsomeApiService} from './ohsome-api.service';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {environment} from '../../environments/environment';
 
-describe('RestService', () => {
+let service: OhsomeApiService;
+let httpMock: HttpTestingController;
+
+describe('OhsomeApiService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientModule],
+      imports: [HttpClientTestingModule],
       providers: [OhsomeApiService]
     });
+
+    service = TestBed.inject(OhsomeApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', inject([OhsomeApiService], (service: OhsomeApiService) => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should be created', () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it('should perform a GET request to ohsome API to a specified endpoint', () => {
+    service.get('test').subscribe(() => {
+    });
+    const req = httpMock.expectOne(`${environment.oshdbRestApiRootUrl}/test`);
+    expect(req.request.method).toEqual('GET');
+  });
+
+  it('should perform a GET request to ohsome API with query params', () => {
+    service.get('test2', 'param=value&param2=value2').subscribe(() => {
+    });
+    const req = httpMock.expectOne(`${environment.oshdbRestApiRootUrl}/test2?param=value&param2=value2`);
+    expect(req.request.method).toEqual('GET');
+  });
+
+  it('should perform a POST request to ohsome API', () => {
+    service.post('test', 'param=value').subscribe(() => {
+    });
+    const req = httpMock.expectOne(`${environment.oshdbRestApiRootUrl}/test`);
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual('param=value');
+  });
+
+  it('should perform a GET request to the ohsome dashboard status page', () => {
+    service.getOhsomeApiAnnouncement().subscribe(() => {
+    });
+    const req = httpMock.expectOne(environment.announcementUrl);
+    expect(req.request.method).toEqual('GET');
+  });
+
 });
