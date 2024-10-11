e8c8562a42af0c9d55052cdb413c06496fd66e1e
Matthias Merdes <matthias.merdes@heigit.org>
2024-10-11 16:21:48 +0200

    build: add new environment/config for TEST system
    Co-authored-by: Michael Auer <michael.auer@heigit.org>

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,30 @@
+export const environment = {
+  production: true,
+
+  //ohsomeAPI (osm history stats from OSHDB)
+  oshdbRestApiRootUrl : 'https://api.ohsome.org/v1',
+
+  //oqtApi (ohsome quality analyst)
+  oqtApiRootUrl: 'https://api.quality.ohsome.org/v1-test/',
+
+  // core, experimental, all, ...
+  oqtApiProject: 'bkg',
+
+  //the WFS endpoint which will get appended a CQL_FILTER="id" IN (number, number, ...) param and should return a GeoJSON<FeatureCollection>
+  ohsomeBoundaryWFSUrl: 'https://maps.heigit.org/ohsome/wfs?service=wfs&request=GetFeature&typeNames=ohsome:admin_world_water&outputFormat=application/json&version=2.0.0&srsName=EPSG:4326',
+
+  // url to fetch an announcement text
+  announcementUrl: 'https://dashboard.ohsome.org/statuspage',
+
+  //map options
+  zoomLevel : 2,
+  mapCenter : {lat: 0.0, lng: 0.0},
+  mapCenterFromPoly : false,
+
+  //ohsomeAPI options
+  selectedFilter : `natural=tree and type:node`,
+  selectedKey : 'natural',
+  selectedValue : 'tree',
+  selectedTypes : ['node'],
+  period : 'P1M',
+};
