e65fcc292fe910b9829389acf6399962e27dca1e
Michael Auer <michael.auer@heigit.org>
2024-10-31 17:18:00 +0100

    feat: rename AttributeCompletenessIndicator to reflect the parameter name to AttributeCompletenessAttributes
    The component simply models the inner parameter form field to carry a list of attributes for the attribute-completeness indicator and is projected (injected) into a simple-indicator component.

--- src/app/oqt/query-form/oqt-api-query-form/attribute-completeness-attributes/attribute-completeness-attributes.component.spec.ts
+++ src/app/oqt/query-form/oqt-api-query-form/attribute-completeness-attributes/attribute-completeness-attributes.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AttributeCompletenessIndicatorComponent } from './attribute-completeness-indicator.component';
+import { AttributeCompletenessAttributesComponent } from './attribute-completeness-attributes.component';
 import {OqtModule} from '../../../oqt.module';
 import {OqtApiMetadataProviderService} from '../../../oqt-api-metadata-provider.service';
 import OqtApiMetadataProviderServiceMock from '../../../oqt-api-metadata-provider.service.mock';
@@ -8,8 +8,8 @@ import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {NgForm} from '@angular/forms';
 
 describe('AttributeCompletenessIndicatorComponent', () => {
-  let component: AttributeCompletenessIndicatorComponent;
-  let fixture: ComponentFixture<AttributeCompletenessIndicatorComponent>;
+  let component: AttributeCompletenessAttributesComponent;
+  let fixture: ComponentFixture<AttributeCompletenessAttributesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,20 +22,13 @@ describe('AttributeCompletenessIndicatorComponent', () => {
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AttributeCompletenessIndicatorComponent);
+    fixture = TestBed.createComponent(AttributeCompletenessAttributesComponent);
     component = fixture.componentInstance;
     component.selectedTopicKey = "roads";
     component.topicName = "Roads";
     component.hashParams = new URLSearchParams("attribute-completeness--attributes=name");
-    component.indicator = {
-      "name": "Attribute Completeness",
-      "description": "Derive the ratio of OSM features compared to features which match additional expected tags (e.g. amenity=hospital vs amenity=hospital and wheelchair=yes).",
-      "projects": ["bkg"],
-      "qualityDimension": "completeness",
-      "key": "attribute-completeness",
-      "checked": true
-    }
-    component.qualityDimension = component.indicator.qualityDimension;
+    component.indicatorKey = "attribute-completeness";
+    component.indicatorChecked = true;
     fixture.detectChanges();
   });
 
