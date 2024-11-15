7c61892a5fb12b91b25b988c78cad9ea079f44a9
Michael Auer <michael.auer@heigit.org>
2024-11-15 18:23:33 +0100

    refactor: move Prism language initialization to APP_INITIALIZER functions to be set at bootstrap and available in all components
    also changed using dependency for Prism from node_modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {APP_INITIALIZER, NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {AppComponent} from './app.component';
 
 import {OshdbModule} from './oshdb/oshdb.module';
@@ -13,60 +13,80 @@ import {SharedModule} from './shared/shared.module';
 import {ResultListDirective} from './result-panel/result-list.directive';
 import {ResultPanelComponent} from './result-panel/result-panel.component';
 import {catchError, EMPTY} from 'rxjs';
+import {PRISM_LANGUAGE_OHSOME_FILTER} from '../prism-language-ohsome-filter';
 
-@NgModule({ declarations: [
-        AppComponent,
-        QueryPanelComponent,
-        ResultPanelComponent,
-        ResultListDirective,
-    ],
-    exports: [
-        QueryPanelComponent,
-        ResultPanelComponent,
-        ResultListDirective
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        SharedModule,
-        OshdbModule,
-        OqtModule], providers: [
-        {
-            provide: APP_INITIALIZER,
-            useFactory: urlHashParamsProviderFactory,
-            deps: [UrlHashParamsProviderService],
-            multi: true
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: ohsomeApiMetadataProviderFactory,
-            deps: [OhsomeApiMetadataProviderService],
-            multi: true
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: ohsomeApiAnnouncementProviderFactory,
-            deps: [OhsomeApiMetadataProviderService],
-            multi: true
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: oqtApiMetadataProviderFactory,
-            deps: [OqtApiMetadataProviderService],
-            multi: true
-        },
-        {
-            provide: APP_INITIALIZER,
-            useFactory: oqtApiAttributeProviderFactory,
-            deps: [OqtApiMetadataProviderService],
-            multi: true
-        },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+declare const Prism;
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QueryPanelComponent,
+    ResultPanelComponent,
+    ResultListDirective,
+  ],
+  exports: [
+    QueryPanelComponent,
+    ResultPanelComponent,
+    ResultListDirective
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    OshdbModule,
+    OqtModule],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: urlHashParamsProviderFactory,
+      deps: [UrlHashParamsProviderService],
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: ohsomeApiMetadataProviderFactory,
+      deps: [OhsomeApiMetadataProviderService],
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: ohsomeApiAnnouncementProviderFactory,
+      deps: [OhsomeApiMetadataProviderService],
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: oqtApiMetadataProviderFactory,
+      deps: [OqtApiMetadataProviderService],
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: oqtApiAttributeProviderFactory,
+      deps: [OqtApiMetadataProviderService],
+      multi: true
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: preparePrismToRenderOhsomeFilterLangauge,
+      multi: true
+    },
+    provideHttpClient(withInterceptorsFromDi()),
+  ]
+})
 export class AppModule {
+}
+
+export function preparePrismToRenderOhsomeFilterLangauge() {
+  // prepare syntax highlighting for ohsome-filter
+  return () : void =>  {
+    Prism.languages['ohsome-filter'] = PRISM_LANGUAGE_OHSOME_FILTER;
+  }
 }
 
 export function ohsomeApiMetadataProviderFactory(provider: OhsomeApiMetadataProviderService) {
   return () => provider.loadOhsomeMetadata().pipe(
-    catchError(()=> EMPTY)
+    catchError(() => EMPTY)
   );
 }
 
