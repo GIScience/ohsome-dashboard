6c3272ec420a8dcb9b1a73c65725f45f9a837216
jstier <stier.jochen@web.de>
2024-11-20 11:37:17 +0100

    feat: make remove button take up more space

--- src/app/oqt/query-form/oqt-api-query-form/attribute-completeness-attributes/attribute-completeness-attributes.component.ts
+++ src/app/oqt/query-form/oqt-api-query-form/attribute-completeness-attributes/attribute-completeness-attributes.component.ts
@@ -17,6 +17,7 @@ declare const $, Prism;
 @Component({
   selector: 'app-attribute-completeness-attributes',
   templateUrl: './attribute-completeness-attributes.component.html',
+  styleUrl: './attribute-completeness-attributes.component.css',
   viewProviders: [{provide: ControlContainer, useExisting: NgForm}],
 })
 export class AttributeCompletenessAttributesComponent implements OnInit, AfterContentInit, OnChanges {
