d2b9f7bc60630879d1e5b8739a6fdeb613827a10
Michael Auer <michael.auer@heigit.org>
2025-10-30 10:11:18 +0100

    feat: change UrlHashParamsProviderService to expose an angular Signal  #74
    Components can now directly react on changes.
    
    "Add welcome screen" https://github.com/GIScience/ohsome-dashboard/issues/74

--- src/app/singelton-services/url-hash-params-provider.service.ts
+++ src/app/singelton-services/url-hash-params-provider.service.ts
@@ -1,23 +1,39 @@
-import { Injectable } from '@angular/core';
+import {Injectable, signal} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UrlHashParamsProviderService {
-  private currentHashParams: URLSearchParams;
+  private _currentHashParams = signal<URLSearchParams>(new URLSearchParams(), {
+    equal: (a, b) => a.toString() === b.toString()
+  });
+  public readonly currentHashParams = this._currentHashParams.asReadonly();
 
   updateHashParamsStoreFromUrl () {
-    this.currentHashParams = new URLSearchParams(window.location.hash.slice(1));//.toLowerCase());
+    this._currentHashParams.update(()=> new URLSearchParams(window.location.hash.slice(1)));//.toLowerCase());
   }
 
+
   getHashURLSearchParams() {
-    if (!this.currentHashParams) this.updateHashParamsStoreFromUrl();
-    return new URLSearchParams(this.currentHashParams);
+    if (this._currentHashParams.length === 0) this.updateHashParamsStoreFromUrl();
+    return this._currentHashParams();
   }
 
   updateHashParams(paramsObject){
-    this.currentHashParams = new URLSearchParams(paramsObject);
-    window.location.hash = this.currentHashParams.toString();
+    this._currentHashParams.update(()=> new URLSearchParams(paramsObject));
+    window.location.hash = this._currentHashParams().toString();
+  }
+
+  updatePartialHashParams(paramsObject){
+    const currentParams = Object.fromEntries(this.currentHashParams().entries());
+    this._currentHashParams.update(()=> new URLSearchParams({...currentParams, ...paramsObject}));
+    window.location.hash = this._currentHashParams().toString();
+  }
+
+  updateHashParam(key:string, value:string) {
+    const currentParams = Object.fromEntries(this.currentHashParams().entries());
+    this._currentHashParams.update(()=> new URLSearchParams({...currentParams, key: value}));
+    window.location.hash = this._currentHashParams().toString();
   }
 
 }
