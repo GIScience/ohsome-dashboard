3b7c6306112ebb76f49b1b9c94223c8ec71d9720
Michael Auer <michael.auer@heigit.org>
2025-10-31 16:13:33 +0100

    feat: improve configurability of prism-code-editor  #74
    Can be used in more style contexts now like in oqt-query-panel or ohsome advanced filter.
    
    "Add welcome screen" https://github.com/GIScience/ohsome-dashboard/issues/74

--- src/app/shared/components/prism-editor/prism-editor.component.ts
+++ src/app/shared/components/prism-editor/prism-editor.component.ts
@@ -14,25 +14,30 @@ import {languages} from 'prism-code-editor/prism';
 import {PRISM_LANGUAGE_OHSOME_FILTER} from '../../../../prism-language-ohsome-filter';
 
 @Component({
-    selector: 'app-prism-editor',
-    imports: [],
-    templateUrl: './prism-editor.component.html'
+  selector: 'app-prism-editor',
+  imports: [],
+  templateUrl: './prism-editor.component.html'
 })
+
 export class PrismEditorComponent implements OnInit, OnChanges {
   @Input() value: string;
-  @Input() readonly: boolean = false
+  @Input() readonly: boolean = false;
+  @Input() partialSetupOptions: Omit<Partial<SetupOptions>, 'value' | 'readOnly' | 'onUpdate'>;
+  @Input() backgroundColor?: string;
+
   @Output() valueChange = new EventEmitter<string>();
   @ViewChild('prismEditorElement', {static: true}) prismEditorElement: ElementRef<HTMLDivElement>;
   editor;
 
-
   ngOnInit(): void {
     // default options for prism editor with ohsome-filter language
     languages['ohsome-filter'] = PRISM_LANGUAGE_OHSOME_FILTER;
+
     const setupOptions: SetupOptions = {
       theme: 'prism',
-      value: this.value,
       language: 'ohsome-filter',
+      ...this.partialSetupOptions,
+      value: this.value,
       onUpdate: (value) => {
         this.valueChange.emit(value);
       }
@@ -45,6 +50,12 @@ export class PrismEditorComponent implements OnInit, OnChanges {
     }
 
     this.editor = basicEditor(this.prismEditorElement.nativeElement, setupOptions);
+
+    if(this.backgroundColor){
+      this.editor.scrollContainer.style.setProperty("--editor__bg", this.backgroundColor);
+      this.editor.scrollContainer.style.setProperty("--editor__bg-highlight", this.backgroundColor);
+    }
+
   }
 
   ngOnChanges(changes: SimpleChanges): void {
