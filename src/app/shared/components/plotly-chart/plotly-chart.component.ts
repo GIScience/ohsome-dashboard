cb408bf0065277ffddfd5ab0521662e31fec1012
Levi Szamek <levi.szamek@heigit.org>
2025-11-06 13:46:00 +0100

    refactor: revert locale for plotly-chart-component

--- src/app/shared/components/plotly-chart/plotly-chart.component.ts
+++ src/app/shared/components/plotly-chart/plotly-chart.component.ts
@@ -5,33 +5,16 @@ import * as Plotly from 'plotly.js-dist-min';
     selector: 'app-plotly-chart',
     templateUrl: './plotly-chart.component.html',
     styleUrls: ['./plotly-chart.component.css'],
-    standalone: false
 })
 export class PlotlyChartComponent implements AfterViewInit{
 @Input() plotlyDataLayoutConfig: Plotly.PlotlyDataLayoutConfig;
 @ViewChild('chart', {static: false}) chartDiv: ElementRef<HTMLDivElement>;
 
-  async ngAfterViewInit(): Promise<void> {
+ngAfterViewInit(): void {
     if (!this.plotlyDataLayoutConfig) return;
-    const locale = localStorage.getItem('locale') || 'en';
-
-    if (locale && locale !== 'en') {
-      try {
-        const localeModule = await import(
-          `plotly.js-locales/${locale}`
-          );
-        (Plotly as any).register(localeModule);
-      } catch (err) {
-        console.warn(`Locale ${locale} not found, falling back to 'en'`, err);
-      }
-    }
 
     const {data, layout, config} = this.plotlyDataLayoutConfig;
 
-    const plotConfig = { ...(config || {}), locale: locale};
-
-    console.log(locale);
-
-    Plotly.newPlot(this.chartDiv.nativeElement,data, layout, plotConfig);
+    Plotly.newPlot(this.chartDiv.nativeElement,data, layout, config);
   }
 }
