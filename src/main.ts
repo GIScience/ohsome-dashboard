e5d7f0039e7d910f0845ca1981c9fbdff3e7afeb
Michael Auer <michael.auer@heigit.org>
2025-11-27 16:18:55 +0100

    chore: update angular to v21
    includes changes from migration scripts, like:
    - Converts the entire application to block control flow syntax
    - Migrates deprecated bootstrap options to providers (and still provide zone based change detection)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 /// <reference types="@angular/localize" />
-
+import { provideZoneChangeDetection } from "@angular/core";
 import {AppModule} from './app/app.module';
 import {platformBrowser} from '@angular/platform-browser';
 import { $localize } from '@angular/localize/init';
 
-platformBrowser().bootstrapModule(AppModule)
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
