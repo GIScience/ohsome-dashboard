e7b69769de22269160441cc6d473d52a3484c65b
Michael Auer <michael.auer@heigit.org>
2025-11-28 12:08:48 +0100

    refactor: remove unused import for localize

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@
 import { provideZoneChangeDetection } from "@angular/core";
 import {AppModule} from './app/app.module';
 import {platformBrowser} from '@angular/platform-browser';
-import { $localize } from '@angular/localize/init';
 
 platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
