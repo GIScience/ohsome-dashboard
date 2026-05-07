df67e52dd87a05d06d0a12aa0830a729922f8834
Johannes Visintini <johannes.visintini@heigit.org>
2026-05-07 18:09:30 +0200

    feat: change defaultTopicKey to cycleway
    Co-authored-by: Michael Auer <michael.auer@heigit.org>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment = {
   oqtApiRootUrl: 'https://api.quality.ohsome.org/v1',
   // core, experimental, all, ...
   oqtApiProject: 'core',
-  defaultTopicKey: 'roads-all-highways',
+  defaultTopicKey: 'cycleway',
 
   //the WFS endpoint which will get appended a CQL_FILTER="id" IN (number, number, ...) param and should return a GeoJSON<FeatureCollection>
   ohsomeBoundaryWFSUrl: 'https://maps.heigit.org/vector/service/ohsome/wfs?service=wfs&request=GetFeature&typeNames=ohsome:admin_world_water&outputFormat=application/json&version=2.0.0&srsName=EPSG:4326',
