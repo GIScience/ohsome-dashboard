0489e465cbd6e11db7fb539a347b1aaeb30fd037
Michael Auer <michael.auer@heigit.org>
2026-06-29 18:15:02 +0200

    chore: add new staging ohsome quality api to be used with UM Keys for local dev environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -11,7 +11,8 @@ export const environment = {
 
   //oqtApi (ohsome quality analyst)
   // oqtApiRootUrl: 'https://api.quality.ohsome.org/v1',
-  oqtApiRootUrl: 'https://staging.api.heigit.org/ohsome-quality-api/v1',
+  // oqtApiRootUrl: 'https://api.quality.ohsome.org/v1-test/',
+  oqtApiRootUrl: 'https://api.heigit.org/ohsome-quality-api-staging/v1',
    // oqtApiRootUrl: 'http://127.0.0.1:8000',
   // core, experimental, all, ...
   oqtApiProject: 'all',
