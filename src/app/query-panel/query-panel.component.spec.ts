cc52df7c4495911bb04bf6dbc499ee4433128d45
Michael Auer <michael.auer@heigit.org>
2026-07-02 15:58:28 +0200

    style: remove unused imports

--- src/app/query-panel/query-panel.component.spec.ts
+++ src/app/query-panel/query-panel.component.spec.ts
@@ -1,13 +1,13 @@
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {QueryPanelComponent} from './query-panel.component';
 import {OhsomeApiMetadataProviderService} from '../ohsomeapi/ohsome-api-metadata-provider.service';
 import OhsomeApiMetadataProviderServiceMock from '../ohsomeapi/ohsome-api-metadata-provider.service.mock';
 import {OshdbModule} from '../ohsomeapi/oshdb.module';
 import {BrowserModule} from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import {provideHttpClient} from '@angular/common/http';
 import {OqtModule} from '../oqapi/oqt.module';
-import {beforeEach, describe, expect, it } from 'vitest';
+import {beforeEach, describe, expect, it} from 'vitest';
 
 describe('QueryPanelComponent', () => {
   let component: QueryPanelComponent;
