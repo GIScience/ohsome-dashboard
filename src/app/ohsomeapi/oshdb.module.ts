88249a2ffb71ba4d580114fe9e7ffdf9cba3f1c3
Michael Auer <michael.auer@heigit.org>
2026-07-10 14:08:48 +0200

    feat(stats): remove SimpleResult Component
    This component was a combination of SimpleChart (chart.js) and some stats numbers.
    It has been replaced by PlotlyChart Component and stats numbers have been dropped

--- src/app/ohsomeapi/oshdb.module.ts
+++ src/app/ohsomeapi/oshdb.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {OhsomeApiService} from './ohsome-api.service';
 import {SimpleChartComponent} from './result/simple-chart/simple-chart.component';
 import {SimpleGroupbyResultComponent} from './result/simple-groupby-result/simple-groupby-result.component';
-import {SimpleResultComponent} from './result/simple-result/simple-result.component';
 import {NgDatePipesModule} from 'ngx-pipes';
 import {TimePeriodPickerInputComponent} from './query-form/time-period-picker-input/time-period-picker-input.component';
 import {OhsomeApiQueryFormComponent} from './query-form/ohsome-api-query-form/ohsome-api-query-form.component';
@@ -14,7 +13,6 @@ import {OhsomeApiQueryFormComponent} from './query-form/ohsome-api-query-form/oh
     NgDatePipesModule,
     SimpleChartComponent,
     SimpleGroupbyResultComponent,
-    SimpleResultComponent,
     TimePeriodPickerInputComponent,
     OhsomeApiQueryFormComponent,
 ],
@@ -22,7 +20,6 @@ import {OhsomeApiQueryFormComponent} from './query-form/ohsome-api-query-form/oh
         OhsomeApiQueryFormComponent,
         SimpleChartComponent,
         SimpleGroupbyResultComponent,
-        SimpleResultComponent,
     ],
     providers: [
         OhsomeApiService,
