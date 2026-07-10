7c518db634c245163bb44bbd8cdb6b797bc9be85
Michael Auer <michael.auer@heigit.org>
2026-07-10 11:21:28 +0200

    fix: do not use authorization for WFS requests to boundaries by id

--- src/app/singelton-services/osm-boundary-provider.service.ts
+++ src/app/singelton-services/osm-boundary-provider.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import {environment} from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpContext} from '@angular/common/http';
 import {FeatureCollection} from 'geojson';
 import {map, Observable, of} from 'rxjs';
+import {SKIP_AUTH} from '../interceptors/skip-auth.token';
 
 const OHSOME_BOUNDARY_WFS_URL = environment.ohsomeBoundaryWFSUrl;
 
@@ -20,7 +21,9 @@ export class OsmBoundaryProviderService {
 
     const idList = ids.join(',');
     const url = `${OHSOME_BOUNDARY_WFS_URL}&CQL_FILTER="id" IN (${idList})`;
-    return this.http.get<FeatureCollection>(url).pipe(map(featureCollection => {
+    return this.http.get<FeatureCollection>(url,{
+      context: new HttpContext().set(SKIP_AUTH, true)
+    }).pipe(map(featureCollection => {
       featureCollection.features.forEach( (feature, index)=> {
         feature['id'] += '-_-' + (feature.properties?.['display_name'] || `area${index}`).replace(/ /g, '__');
       });
