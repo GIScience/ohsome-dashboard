e366a5102984eee6bb6f446f388930a75c61691f
Michael Auer <michael.auer@heigit.org>
2026-07-28 13:41:54 +0200

    feat(extraction): display result with download links

--- src/app/extraction/result/extraction-result.component.ts
+++ src/app/extraction/result/extraction-result.component.ts
@@ -1,6 +1,8 @@
-import {Component, ComponentRef, OnDestroy} from '@angular/core';
+import {Component, ComponentRef, computed, inject, input, OnDestroy} from '@angular/core';
 import {environment} from '../../../environments/environment';
-
+import {ExtractionFormData} from '../query-form/types';
+import {paths} from '../../shared/ohsome-api-v2-types';
+import {OqtApiMetadataProviderService} from '../../oqapi/oqt-api-metadata-provider.service';
 
 @Component({
   selector: 'app-extraction-result',
@@ -9,11 +11,50 @@ import {environment} from '../../../environments/environment';
   styleUrl: './extraction-result.component.css',
 })
 export class ExtractionResultComponent implements OnDestroy {
+  oqtApiMetadataProviderService = inject(OqtApiMetadataProviderService);
+  topics = this.oqtApiMetadataProviderService.getOqtApiMetadata().result.topics;
 
   componentRef: ComponentRef<ExtractionResultComponent>;
+  formValues = input.required<ExtractionFormData>();
+
+  private downloadParams = computed(()=>{
+    return this.buildDownloadParams(this.formValues());
+  })
+
+  getDownloadUrl(format: 'parquet' | 'arrow') {
+    // normalize trailing slash
+    const ohsomeApiRootUrl = environment.ohsomeApiRootUrl.endsWith('/') ? environment.ohsomeApiRootUrl : `${environment.ohsomeApiRootUrl}/`
+
+    const pathUrl = new URL(`extraction/features.${format}`, ohsomeApiRootUrl);
+    this.downloadParams().forEach((value, key) => {
+      pathUrl.searchParams.set(key, value);
+    })
+    return new URL(pathUrl).toString();
+  }
 
   ngOnDestroy(): void {
     this.componentRef.destroy();
+  }
+
+  buildDownloadParams(formValues: ExtractionFormData) {
+    type ExtractionQueryParams = paths['/extraction/features.parquet']['get']['parameters']['query'];
+    const params:ExtractionQueryParams = {
+      aoi: formValues.aoi as string,
+      clip: formValues.clip,
+      timestamp: formValues.timestamp,
+      filter: this.getFilterFromFormVaulues(formValues),
+    }
+    // @ts-ignore
+    return new URLSearchParams(params);
+
+  }
+
+  getFilterFromFormVaulues(formValues: ExtractionFormData) {
+    if (formValues.topic === 'custom-topic') {
+      return formValues['topic-filter']
+    }
+
+    return this.oqtApiMetadataProviderService.getOqtApiMetadata().result.topics[formValues.topic].filter ?? '';
   }
 
   protected readonly environment = environment;
--- src/app/result-panel/result-list.directive.ts
+++ src/app/result-panel/result-list.directive.ts
@@ -4,56 +4,55 @@ import {DataService} from '../singelton-services/data.service';
 import {OqtResultComponent} from '../oqapi/result/oqt-result.component';
 import {ExtractionResultComponent} from '../extraction/result/extraction-result.component';
 
-@Directive({
-  selector: '[appResultList]'
-})
-export class ResultListDirective implements OnInit {
-  private container = inject(ViewContainerRef);
-  private dataService = inject(DataService);
+  @Directive({
+    selector: '[appResultList]'
+  })
+  export class ResultListDirective implements OnInit {
+    private container = inject(ViewContainerRef);
+    private dataService = inject(DataService);
 
-  private resultItem: ComponentRef<ResultComponent>;
+    private resultItem: ComponentRef<ResultComponent>;
 
-  get length() {
-    return this.container.length;
+    get length() {
+      return this.container.length;
+    }
+
+    ngOnInit() {
+      this.dataService.currentFormValues.subscribe(result => {
+        const backend = result.formValues.backend;
+        switch (backend) {
+          case 'ohsomeApi':
+            this.createResultComponent(result);
+            break;
+          case 'oqtApi':
+            this.createOqtComponent(result);
+            break;
+          default:
+            console.log("create extraction result", result);
+            this.createExtractionComponent(result);
+        }
+
+      });
+    }
+
+    createResultComponent(result) {
+      this.resultItem = this.container.createComponent(ResultComponent, {index: 0});
+      this.resultItem.instance.formValues = result.formValues;
+      this.resultItem.instance.boundaryType = result.boundaryType;
+      this.resultItem.instance.componentRef = this.resultItem;
+    }
+
+    private createOqtComponent(result) {
+      const oqtResultItem = this.container.createComponent(OqtResultComponent, {index: 0});
+      oqtResultItem.instance.formValues = result.formValues;
+      oqtResultItem.instance.boundaryType = result.boundaryType;
+      oqtResultItem.instance.componentRef = oqtResultItem;
+    }
+
+    private createExtractionComponent(result) {
+      const extractionResultItem = this.container.createComponent(ExtractionResultComponent, {index: 0});
+      extractionResultItem.setInput('formValues', result.formValues);
+      extractionResultItem.instance.componentRef = extractionResultItem;
+    }
+
   }
-
-  ngOnInit() {
-    this.dataService.currentFormValues.subscribe(result => {
-      const backend = result.formValues.backend;
-      switch (backend) {
-        case 'ohsomeApi':
-          this.createResultComponent(result);
-          break;
-        case 'oqtApi':
-          this.createOqtComponent(result);
-          break;
-        default:
-          console.log("create extraction result", result);
-          this.createExtractionComponent(result);
-      }
-
-    });
-  }
-
-  createResultComponent(result) {
-    this.resultItem = this.container.createComponent(ResultComponent, {index: 0});
-    this.resultItem.instance.formValues = result.formValues;
-    this.resultItem.instance.boundaryType = result.boundaryType;
-    this.resultItem.instance.componentRef = this.resultItem;
-  }
-
-  private createOqtComponent(result) {
-    const oqtResultItem = this.container.createComponent(OqtResultComponent, {index: 0});
-    oqtResultItem.instance.formValues = result.formValues;
-    oqtResultItem.instance.boundaryType = result.boundaryType;
-    oqtResultItem.instance.componentRef = oqtResultItem;
-  }
-
-  private createExtractionComponent(result) {
-    const extractionResultItem = this.container.createComponent(ExtractionResultComponent, {index: 0});
-    // oqtResultItem.instance.formValues = result.formValues;
-    // oqtResultItem.instance.boundaryType = result.boundaryType;
-    extractionResultItem.instance.componentRef = extractionResultItem;
-  }
-
-}
