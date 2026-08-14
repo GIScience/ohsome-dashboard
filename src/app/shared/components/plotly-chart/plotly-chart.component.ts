e868ff65cee6517eb259a0bae27cfebf98c34363
Michael Auer <michael.auer@heigit.org>
2026-08-14 08:52:43 +0200

    feat: make all plotly charts responsive to resizing of browser window

--- src/app/shared/components/plotly-chart/plotly-chart.component.ts
+++ src/app/shared/components/plotly-chart/plotly-chart.component.ts
@@ -1,5 +1,6 @@
 import {afterRenderEffect, ChangeDetectionStrategy, Component, ElementRef, input, viewChild} from '@angular/core';
 import Plotly from 'plotly.js-dist-min';
+import Utils from '../../../../utils';
 
 @Component({
   selector: 'app-plotly-chart',
@@ -16,6 +17,13 @@ export class PlotlyChartComponent {
   constructor() {
     afterRenderEffect(() => {
       const cfg = this.plotlyDataLayoutConfig();
+
+      // make chart responsive
+      Utils.setObjectProperty(cfg, 'config.responsive', true);
+      Utils.setObjectProperty(cfg, 'layout.autosize', true);
+
+      // make sure the title of yaxis has enough space
+      Utils.setObjectProperty(cfg, 'layout.yaxis.automargin', true);
 
       if (!cfg || !this.chartDiv().nativeElement) return;
 
