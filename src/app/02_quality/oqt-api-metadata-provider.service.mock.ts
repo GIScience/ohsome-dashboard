e40321450885260017f22c00488e9abfcb0433f7
Michael Auer <michael.auer@heigit.org>
2026-08-14 11:33:29 +0200

    fix: tests

--- src/app/02_quality/oqt-api-metadata-provider.service.mock.ts
+++ src/app/02_quality/oqt-api-metadata-provider.service.mock.ts
@@ -10,13 +10,15 @@ const OqtApiMetadataProviderServiceMock = {
   'getAttributeDescription': vi.fn(),
   'getAttributeFilter': vi.fn(),
   'getTopicMeasure': vi.fn(),
-  'getTopicFilter': vi.fn()
+  'getTopicFilter': vi.fn(),
+  'getTopicName': vi.fn()
 };
 
 OqtApiMetadataProviderServiceMock.loadOqtApiMetadata.mockReturnValue(of(oqtApiMetadataResponseMock))
 OqtApiMetadataProviderServiceMock.getOqtApiMetadata.mockReturnValue(oqtApiMetadataResponseMock);
 OqtApiMetadataProviderServiceMock.getAttributes.mockReturnValue(oqtAttributesResponseMock);
 OqtApiMetadataProviderServiceMock.getTopicMeasure.mockReturnValue('count');
+OqtApiMetadataProviderServiceMock.getTopicName.mockReturnValue('Cycleway');
 
 export default OqtApiMetadataProviderServiceMock;
 
